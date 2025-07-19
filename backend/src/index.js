489e5504c8115b17afeee279f8ef37614875f0f5
Rym Bachrouch <icanseeyou9905@gmail.com>
2025-07-19 17:49:14 +0100

    Fixed reference

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.send.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 
